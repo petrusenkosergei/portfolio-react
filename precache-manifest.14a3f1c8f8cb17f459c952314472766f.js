c9bc5be12aa5e81243a1cf2fd9957eb91b22a8e9
Sergei <sergei_petrusenko@mail.ru>
2019-10-13 14:39:09 +0300

    Updates

---
+++ precache-manifest.14a3f1c8f8cb17f459c952314472766f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087e713ad555cda05a2ea13f4d35efdc",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "0d8ed618d47205520d0a",
+    "url": "/portfolio-react/static/css/main.795741fd.chunk.css"
+  },
+  {
+    "revision": "61aae5aa9ba1e4f6eb40",
+    "url": "/portfolio-react/static/js/2.d085fb2e.chunk.js"
+  },
+  {
+    "revision": "0d8ed618d47205520d0a",
+    "url": "/portfolio-react/static/js/main.eed92491.chunk.js"
+  },
+  {
+    "revision": "d3a28c8f93449708e95f",
+    "url": "/portfolio-react/static/js/runtime~main.5011f639.js"
+  },
+  {
+    "revision": "68466e9ab763dd91acc8d401187af1b5",
+    "url": "/portfolio-react/static/media/Avatar.68466e9a.jpg"
+  },
+  {
+    "revision": "ff60d17e87eb74160dd67b176a9e75db",
+    "url": "/portfolio-react/static/media/Count.ff60d17e.png"
+  },
+  {
+    "revision": "e0b396601d91eddd2b9b31f04c8560c4",
+    "url": "/portfolio-react/static/media/Instagram_icon.e0b39660.png"
+  },
+  {
+    "revision": "6b6290a683a80e62efcb707fbef5bba7",
+    "url": "/portfolio-react/static/media/Linkedin_icon.6b6290a6.png"
+  },
+  {
+    "revision": "392355aa36a5ab069cbca832bf6f7c99",
+    "url": "/portfolio-react/static/media/VK_icon.392355aa.png"
+  },
+  {
+    "revision": "771551b909fd715c3a185f8e86225bb8",
+    "url": "/portfolio-react/static/media/gulp.771551b9.svg"
+  },
+  {
+    "revision": "ce6e5ba07db5030872f1c4934b16a166",
+    "url": "/portfolio-react/static/media/html_icon.ce6e5ba0.svg"
+  },
+  {
+    "revision": "6e8f68df1509ab98bffab08b6f001c56",
+    "url": "/portfolio-react/static/media/react-icon.6e8f68df.png"
+  },
+  {
+    "revision": "034768c860323a5ff87673582200fb70",
+    "url": "/portfolio-react/static/media/react_todo_list.034768c8.jpg"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/portfolio-react/static/media/redux.59c46753.png"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/portfolio-react/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "4fb728226b7662381ecdaa9fa2f81eb9",
+    "url": "/portfolio-react/static/media/social_icons.4fb72822.jpg"
+  }
+]);
