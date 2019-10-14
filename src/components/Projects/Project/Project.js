87c0ea504a3c463082a81f1d85803c7eadf88bf7
Sergei <sergei_petrusenko@mail.ru>
2019-10-15 00:07:42 +0300

    github pages (button5)

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -9,8 +9,10 @@ import styles from './Project.module.css';
                 <div className={styles.projectImg} style={props.style}></div>
                 <div className={styles.projectTitle}>{props.title}</div>
                 <div className={styles.description}>{props.description}</div>
+                <div className={styles.btn}>
                 <a className={styles.btnShowCode} href=""><span className={styles.btnTitle} >Смотреть код</span></a>
                 <a className={styles.btnShow} href=""><span className={styles.btnTitle}>Смотреть проект</span></a>
+                </div>
             </div>
         );
     }
